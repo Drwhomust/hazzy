d1f71c63394180beb63cbc96f2b6c3c0f33bacc6
Drwhomust <rabbids8008@gmail.com>
2025-03-09 13:14:21 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,8 +1,16 @@
 function love.load()
+    anim8 = require 'libraries/anim8'
+
     player = {}
     player.x = 400 -- starting point
     player.y = 200
     player.speed = 3 -- speed of player
+
+    player.spritesheet = love.graphics.newImage('sprites/hazzy.png')
+    player.grid = anim8.newGrid( 47, 61, player.spritesheet:getWidth(), player.spritesheet:getHeight() )
+
+    player.animations = {}
+    player.animations.down = anim8.newAnimation( player.grid('1-3', 1), 0.2 )
 end
 
 function love.update(dt)
@@ -21,8 +29,10 @@ function love.update(dt)
     if love.keyboard.isDown("up") then -- if up key is held down then move up
         player.y = player.y - player.speed
     end
+
+    player.animations.down:update(dt)
 end
 
 function love.draw()
-    love.graphics.circle("fill", player.x, player.y, 100)
+    player.animations.down:draw(player.spritesheet, player.x, player.y)
 end
