802c64e84e9180c0b6407f878e7cef3aea4e341a
Drwhomust <rabbids8008@gmail.com>
2025-03-09 12:52:11 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,26 @@
 function love.load()
     player = {}
-    player.x = 400
+    player.x = 400 -- starting point
     player.y = 200
+    player.speed = 3 -- speed of player
 end
 
 function love.update(dt)
+    if love.keyboard.isDown("right") then -- if right key is held down then move right
+        player.x = player.x + player.speed
+    end
 
+    if love.keyboard.isDown("left") then -- if left key is held down then move left
+        player.x = player.x - player.speed
+    end
+
+    if love.keyboard.isDown("down") then -- if down key is held down then move down
+        player.y = player.y + player.speed
+    end
+
+    if love.keyboard.isDown("up") then -- if up key is held down then move up
+        player.y = player.y - player.speed
+    end
 end
 
 function love.draw()
