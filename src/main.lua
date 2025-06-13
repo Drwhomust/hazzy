34a54f0ca8af04ab94fdb0d2b36b085f9e0c7095
Drwhomust <rabbids8008@gmail.com>
2025-06-13 15:58:31 -0700

    made improvements

--- src/main.lua
+++ src/main.lua
@@ -1,5 +1,5 @@
 function love.load()
-  debug = 0 -- why the hell do we have a debug mode when it does nothing to debug
+  debug = 1
   debugnumber = 1
 
   player = {}
@@ -7,29 +7,49 @@ function love.load()
   player.y = 100
   player.canwalk = true
   player.face = "south"
+  player.iswalking = false
 
 end
 
 function love.update(dt)
-  if debug == 1 then
-    debugnumber = debugnumber + 1
-  end
 
  if love.keyboard.isDown("right") then
   if player.canwalk == true then
-    player.x = player.x + 20
+    player.x = player.x + 8
+    player.face = "east"
+    player.iswalking = true
+    else
+      player.walking = false
   end
 end
 
-if love.keyboard.isDown("left") then
-if player.canwalk == true then
-player.x = player.x - 20
-end
+  if love.keyboard.isDown("left") then
+    if player.canwalk == true then
+      player.x = player.x - 8
+      player.face = "west"
+      player.iswalking = true
+      else
+      player.walking = false
+  end
 end
 
   if love.keyboard.isDown("up") then
     if player.canwalk == true then
-    player.x = player.y - 20
+      player.y = player.y - 8
+      player.face = "north"
+      player.iswalking = true
+    else
+      player.walking = false
+    end
+end
+
+  if love.keyboard.isDown("down") then
+    if player.canwalk == true then
+      player.y = player.y + 8
+      player.face = "south"
+      player.iswalking = true
+    else
+      player.walking = false
   end
 end
 
@@ -37,6 +57,15 @@ end
 
 function love.draw()
   if debug == 1 then
-    love.graphics.print(debugnumber, 10, 200)
+    love.graphics.print(player.x, 0, 10)
+    love.graphics.print(player.y, 0, 20)
+    love.graphics.print(player.face, 0, 30)
+    if player.iswalking == true then
+      love.graphics.print("The player is walking", 0, 40)
+    else
+      love.graphics.print("the player is NOT walking", 0, 40)
+    end
   end
+
+  love.graphics.rectangle("fill", player.x, player.y, 100, 100)
 end
