8c9f85737e5a0e976a78f9bf495bc09eefcff70b
Drwhomust <rabbids8008@gmail.com>
2025-06-13 10:32:14 -0700

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,12 +1,38 @@
 function love.load()
-  debug = 1
+  debug = 1 -- why the hell do we have a debug mode when it does nothing to debug
   debugnumber = 1
+
+player = {}
+player.x = 100
+player.y = 100
+player.canwalk = true
+player.face = "south"
+
 end
 
 function love.update(dt)
   if debug == 1 then
     debugnumber = debugnumber + 1
   end
+
+ if love.keyboard.isDown("right") then
+if player.canwalk == true then
+player.x = player.x + 20
+end
+end
+
+if love.keyboard.isDown("left") then
+if player.canwalk == true then
+player.x = player.x - 20
+end
+end
+
+if love.keyboard.isDown("up") then
+if player.canwalk == true then
+player.x = player.y - 20
+end
+end
+
 end
 
 function love.draw()
