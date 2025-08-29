3b0fc6fce9f5ad21771c767dd6304674e39834ba
Drwhomust <rabbids8008@gmail.com>
2025-08-29 13:50:08 -0700

    Update main.js

--- hazzy/resources/js/main.js
+++ hazzy/resources/js/main.js
@@ -8,6 +8,16 @@ window.onload = function() {
     SceneManager.run(Scene_Boot);
 };
 
+var options = {
+	version: "1.0.0",
+	checkHostLicense: true,
+    	autoLogNewView: true,
+    	preloadMedals: true,
+};
+
+NGIO.init(appID, aesKey, options);
+// get your own fucking api key
+
 Neutralino.init();
 Neutralino.events.on("windowClose", () => {
   Neutralino.app.exit();
