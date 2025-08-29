ac1f873b397ddb57d29c25fd6ff25640acba2901
Drwhomust <rabbids8008@gmail.com>
2025-08-28 19:13:14 -0700

    window thingy

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,8 @@ PluginManager.setup($plugins);
 window.onload = function() {
     SceneManager.run(Scene_Boot);
 };
+
+Neutralino.init();
+Neutralino.events.on("windowClose", () => {
+  Neutralino.app.exit();
+});
