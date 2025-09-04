13f4a6cb2467d68c393dacc93321e48cf9c972fc
Drwhomust <rabbids8008@gmail.com>
2025-09-03 23:23:42 -0700

    Update main.js

--- hazzy/resources/js/main.js
+++ hazzy/resources/js/main.js
@@ -8,13 +8,6 @@ window.onload = function() {
     SceneManager.run(Scene_Boot);
 };
 
-var options = {
-	version: "1.0.0",
-	checkHostLicense: true,
-    	autoLogNewView: true,
-    	preloadMedals: true,
-};
-
 Neutralino.init();
 Neutralino.events.on("windowClose", () => {
   Neutralino.app.exit();
