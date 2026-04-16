fd4fbe44d62ff2f8b488f7d5bd2675710e90bf6e
Drwhomust <rabbids8008@gmail.com>
2026-04-16 14:22:21 -0700

    added firing tool and also other stuff

--- main-source-code/main.lua
+++ main-source-code/main.lua
@@ -9,12 +9,16 @@ print("Loading........")
 -- loads libaries
 -- dev note. dispite me maining coding in snake-case. these libraires keep using camleCase and other stuff
 -- please fix this
-local SM = require("libs/scene-stuff/SceneMgr/lib/SceneMgr")
-
+local SM = require("libs/scene-stuff/SceneMgr/lib/SceneMgr") -- scene manger. refer to it's docs for more info
+local fire = require("libs/hump/signal") -- this is for the singal libray from hump. it's called fire because it acts like the fire events from roblox.
 
 -- loads assets into game
 function love.load()
     SM.setPath("scenes/") -- scene path. needed when loading maps
+end
+
+function love.update(dt)
+
 end
 
 function love.draw()
