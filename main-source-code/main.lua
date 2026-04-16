bc9a2aa499d0ecbdcf316ca8d5174a612e75a476
Drwhomust <rabbids8008@gmail.com>
2026-04-16 11:49:03 -0700

    I tried to do some stuff. it didn't work but i will keep trying!

--- main-source-code/main.lua
+++ main-source-code/main.lua
@@ -6,8 +6,19 @@
 
 print("Loading........")
 
-SM = require("libs/scene-stuff/SceneMgr/lib/SceneMgr")
+-- loads libaries
+-- dev note. dispite me maining coding in snake-case. these libraires keep using camleCase and other stuff
+-- please fix this
+local SM = require("libs/scene-stuff/SceneMgr/lib/SceneMgr")
 
+
+-- loads assets into game
 function love.load()
-    SM.setPath("scenes/")
+    SM.setPath("scenes/") -- scene path. needed when loading maps
 end
+
+function love.draw()
+    love.graphics.print("hi hi",50,59)
+end
+
+print("Finished loading!")
