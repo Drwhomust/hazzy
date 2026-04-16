e8b789562e044f7b3d37ddfa07778b2cd3ca3d4d
Drwhomust <rabbids8008@gmail.com>
2026-04-15 22:21:52 -0700

    got something done

---
+++ main-source-code/main.lua
@@ -0,0 +1,13 @@
+--[[ hazzy main script
+        written by: Drwhomust
+            The silly lizard :3
+                (they won't infect ya)
+]]
+
+print("Loading........")
+
+SM = require("libs/scene-stuff/SceneMgr/lib/SceneMgr")
+
+function love.load()
+    SM.setPath("scenes/")
+end
