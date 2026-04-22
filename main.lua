e2fc1413e5665c63ebd31f0449e07603f69de4db
Drwhomust <rabbids8008@gmail.com>
2026-04-22 13:24:09 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+--[[ a mini lua file JUST to load the main script. if you
+    are using git sub modulers this will be useful as
+    you don't have to point love the the main-source-code 
+    folder
+]]
+require("main-source-code/main")
