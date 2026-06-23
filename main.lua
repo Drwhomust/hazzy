78ea9464e0f30b3288720ac4a4992dcf243ea94f
Drwhomust <rabbids8008@gmail.com>
2026-06-23 01:12:46 -0700

    nuke
    this time i am locking in this time trust i will do it this time!

--- main.lua
+++
@@ -1,8 +0,0 @@
---[[ a mini lua file JUST to load the main script. if you
-    are using git sub modulers this will be useful as
-    you don't have to point love the the main-source-code 
-    folder
-]]
-require("main-source-code/main")
-
--- ok bye
